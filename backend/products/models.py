f281fa8494fe78b846c4a4e4d6652593448a6291
Elder Santos <elder_rn@hotmail.com>
2023-06-03 13:41:19 -0300

    fix: add min/max constraints to unit_price and commission_rate in Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
+
 from common.validators import only_numbers_validator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -9,8 +12,20 @@ class Product(models.Model):
         validators=[only_numbers_validator],
     )
     description = models.CharField(max_length=255)
-    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
-    commission_rate = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    unit_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("1"))],
+    )
+    commission_rate = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        default=0,
+        validators=[
+            MinValueValidator(Decimal("0")),
+            MaxValueValidator(Decimal("0.1")),
+        ],
+    )
 
     def __str__(self):
         return f"{self.code} - {self.description}"
