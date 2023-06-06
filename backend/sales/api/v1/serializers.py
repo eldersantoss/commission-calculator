24dc1fb79e13f84b4ab193d0c2710bb69e42199e
Elder Santos <elder_rn@hotmail.com>
2023-06-05 21:48:14 -0300

    feat: add SaleProductSerializer and SaleSerializer

---
+++ backend/sales/api/v1/serializers.py
@@ -0,0 +1,63 @@
+from persons.models import Customer, Vendor
+from products.models import Product
+from rest_framework import serializers
+
+from ...models import Sale, SaleProduct
+
+
+class SaleProductSerializer(serializers.ModelSerializer):
+    product = serializers.HyperlinkedRelatedField(
+        view_name="product-detail",
+        queryset=Product.objects.all(),
+    )
+
+    class Meta:
+        model = SaleProduct
+        fields = ["product", "quantity"]
+
+
+class SaleSerializer(serializers.ModelSerializer):
+    customer = serializers.HyperlinkedRelatedField(
+        view_name="customer-detail",
+        queryset=Customer.objects.all(),
+    )
+    vendor = serializers.HyperlinkedRelatedField(
+        view_name="vendor-detail",
+        queryset=Vendor.objects.all(),
+    )
+    products = SaleProductSerializer(many=True, source="saleproduct_set")
+
+    class Meta:
+        model = Sale
+        fields = [
+            "invoice_number",
+            "date_time",
+            "customer",
+            "vendor",
+            "products",
+        ]
+
+    def create(self, validated_data):
+        products_data = validated_data.pop("saleproduct_set", [])
+        sale = Sale.objects.create(**validated_data)
+
+        for product_data in products_data:
+            SaleProduct.objects.create(sale=sale, **product_data)
+
+        return sale
+
+    def update(self, instance, validated_data):
+        products_data = validated_data.pop("saleproduct_set", [])
+        instance.invoice_number = validated_data.get(
+            "invoice_number", instance.invoice_number
+        )
+        instance.date_time = validated_data.get("date_time", instance.date_time)
+        instance.customer = validated_data.get("customer", instance.customer)
+        instance.vendor = validated_data.get("vendor", instance.vendor)
+
+        instance.saleproduct_set.all().delete()
+
+        for product_data in products_data:
+            SaleProduct.objects.create(sale=instance, **product_data)
+
+        return instance
---
+++ backend/sales/tests/test_serializers.py
@@ -0,0 +1,178 @@
+from django.test import TestCase
+from django.utils import timezone
+from model_mommy import mommy
+
+from ..api.v1.serializers import SaleProductSerializer, SaleSerializer
+from ..models import Sale, SaleProduct
+from .data import TestDataMixin
+
+
+class SaleProductSerializerTests(TestDataMixin, TestCase):
+    def test_valid_serialization(self):
+        """
+        Serializer should create a serialized representation of the model instance with provided data
+        """
+
+        sale_products = SaleProduct.objects.all()
+        serializer = SaleProductSerializer(
+            sale_products,
+            many=True,
+            context=self.serializer_context,
+        )
+        self.assertEqual(len(serializer.data), sale_products.count())
+        for data in serializer.data:
+            self.assertIn("product", data)
+            self.assertIn("quantity", data)
+
+
+class SaleSerializerTests(TestDataMixin, TestCase):
+    def test_valid_serialization(self):
+        """
+        Serializer should create a serialized representation of the model instance with provided data
+        """
+
+        sale_data = {
+            "invoice_number": self.invoice_number,
+            "date_time": self.date_time,
+            "customer": "http://testserver" + self.customer.get_absolute_url(),
+            "vendor": "http://testserver" + self.vendor.get_absolute_url(),
+            "products": [
+                {
+                    "product": "http://testserver"
+                    + self.products[0].get_absolute_url(),
+                    "quantity": 1,
+                },
+                {
+                    "product": "http://testserver"
+                    + self.products[1].get_absolute_url(),
+                    "quantity": 2,
+                },
+                {
+                    "product": "http://testserver"
+                    + self.products[2].get_absolute_url(),
+                    "quantity": 3,
+                },
+            ],
+        }
+
+        serializer = SaleSerializer(self.sale, context=self.serializer_context)
+
+        self.assertEqual(serializer.data, sale_data)
+
+    def test_valid_deserialization(self):
+        """
+        Serializer should be valid and create the model instance with provided data
+        """
+
+        sale_data = {
+            "invoice_number": "0000000002",
+            "date_time": "2023-06-04T21:12:45.529818-03:00",
+            "customer": "http://testserver" + self.customer.get_absolute_url(),
+            "vendor": "http://testserver" + self.vendor.get_absolute_url(),
+            "products": [
+                {
+                    "product": "http://testserver"
+                    + self.products[0].get_absolute_url(),
+                    "quantity": 5,
+                },
+                {
+                    "product": "http://testserver"
+                    + self.products[1].get_absolute_url(),
+                    "quantity": 5,
+                },
+            ],
+        }
+
+        serializer = SaleSerializer(data=sale_data)
+        self.assertTrue(serializer.is_valid())
+
+        sale = serializer.save()
+        self.assertEqual(sale.invoice_number, sale_data["invoice_number"])
+        self.assertEqual(sale.date_time.isoformat(), sale_data["date_time"])
+        self.assertEqual(
+            "http://testserver" + sale.customer.get_absolute_url(),
+            sale_data["customer"],
+        )
+        self.assertEqual(
+            "http://testserver" + sale.vendor.get_absolute_url(),
+            sale_data["vendor"],
+        )
+
+        sale_products = SaleProduct.objects.filter(sale=sale)
+        serializer = SaleProductSerializer(
+            sale_products, many=True, context=self.serializer_context
+        )
+        self.assertEqual(serializer.data, sale_data["products"])
+
+    def test_instance_update(self):
+        """
+        Serializer should be valid and update the model instance with provided data
+        """
+
+        sale = mommy.make(Sale, invoice_number="0000000010")
+
+        now = timezone.now()
+        desired_timezone = timezone.get_default_timezone()
+        adjusted_now = now.astimezone(desired_timezone)
+        updated_date_time = adjusted_now.isoformat()
+
+        updated_sale_data = {
+            "invoice_number": sale.invoice_number,
+            "date_time": updated_date_time,
+            "customer": "http://testserver" + self.customer.get_absolute_url(),
+            "vendor": "http://testserver" + self.vendor.get_absolute_url(),
+            "products": [
+                {
+                    "product": "http://testserver"
+                    + self.products[0].get_absolute_url(),
+                    "quantity": 5,
+                },
+            ],
+        }
+
+        serializer = SaleSerializer(instance=sale, data=updated_sale_data)
+        self.assertTrue(serializer.is_valid())
+
+        updated_sale = serializer.save()
+        self.assertEqual(
+            updated_sale.date_time.isoformat(),
+            updated_sale_data["date_time"],
+        )
+        self.assertEqual(updated_sale.products.count(), 1)
+
+    def test_invalid_deserialization(self):
+        """
+        Serializer should be invalid for missing or invalid data
+        """
+
+        invalid_data = [
+            {
+                "invoice_number": "",
+                "date_time": "2023-06-04T21:12:45.529818-03:00",
+                "customer": "http://testserver" + self.customer.get_absolute_url(),
+                "vendor": "http://testserver" + self.vendor.get_absolute_url(),
+                "products": [],
+            },
+            {
+                "invoice_number": "0000000003",
+                "date_time": "2023-06-04T21:12:45.529818-03:00",
+                "customer": "http://testserver",
+                "vendor": "http://testserver" + self.vendor.get_absolute_url(),
+                "products": [],
+            },
+            {
+                "invoice_number": "0000000004",
+                "date_time": "2023-06-04T21:12:45.529818-03:00",
+                "customer": "http://testserver" + self.customer.get_absolute_url(),
+                "vendor": "http://testserver" + self.vendor.get_absolute_url(),
+                "products": [
+                    {
+                        "product": "",
+                    },
+                ],
+            },
+        ]
+
+        for data in invalid_data:
+            serializer = SaleSerializer(data=data)
+            self.assertFalse(serializer.is_valid())
