3f50b3c9737b7a4729646fd7d4239cab6a1579e5
Elder Santos <elder_rn@hotmail.com>
2023-06-12 15:49:28 -0300

    feat: add CommissionsDataContext

---
+++ frontend/src/hooks/useCommissionsDataContext.ts
@@ -0,0 +1,8 @@
+
+import { CommisionsDataContext } from "@/contexts/CommissionsDataContext";
+import { useContext } from "react";
+
+
+const useCommissionsDataContext = () => useContext(CommisionsDataContext)
+
+export default useCommissionsDataContext
