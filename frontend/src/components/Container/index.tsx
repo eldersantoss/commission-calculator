8f7def2e754187c696e552cd5795b0ac29164a51
Elder Santos <elder_rn@hotmail.com>
2023-06-12 15:56:34 -0300

    refactor: update formatting of Container and MainContent components

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -1,18 +1,19 @@
 import { ReactNode } from "react";
 
-
 interface ContainerProps {
   children: ReactNode;
 }
 
-export default function Container({ children }: ContainerProps ) {
+export default function Container({ children }: ContainerProps) {
   return (
-    <main style={{
-      display: "flex",
-      height: "100vh",
-      width: "100vw",
-    }}>
+    <main
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
       {children}
     </main>
-  )
+  );
 }
---
+++ frontend/src/components/PageActions/index.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+
+interface PageActionsProps {
+  children: ReactNode;
+}
+
+export default function PageActions({ children }: PageActionsProps ) {
+  return (
+    <div style={{
+      width: "100%",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      flexWrap: "wrap",
+      marginBottom: "70px",
+    }}>
+      {children}
+    </div>
+  )
+}
