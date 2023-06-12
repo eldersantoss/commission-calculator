d5c0d430b27123b1315e3957e25c2d1a9066b512
Elder Santos <elder_rn@hotmail.com>
2023-06-12 17:40:54 -0300

    feat: add some utils methods to Sale model

--- backend/sales/api/v1/serializers.py
+++ backend/sales/api/v1/serializers.py
@@ -16,7 +16,7 @@ class SaleProductSerializer(serializers.ModelSerializer):
         fields = ["product", "quantity"]
 
 
-class SaleSerializer(serializers.ModelSerializer):
+class SaleSerializer(serializers.HyperlinkedModelSerializer):
     customer = serializers.HyperlinkedRelatedField(
         view_name="customer-detail",
         queryset=Customer.objects.all(),
@@ -27,18 +27,46 @@ class SaleSerializer(serializers.ModelSerializer):
     )
     products = SaleProductSerializer(many=True, source="saleproduct_set")
 
+    vendor_name = serializers.SerializerMethodField(read_only=True)
+    customer_name = serializers.SerializerMethodField(read_only=True)
+    total_value = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Sale
         fields = [
+            "id",
+            "url",
             "invoice_number",
             "date_time",
             "customer",
             "vendor",
             "products",
+            "customer_name",
+            "vendor_name",
+            "total_value",
         ]
+        read_only_fields = [
+            "id",
+            "url",
+            "invoice_number",
+            "vendor_name",
+            "customer_name",
+            "total_value",
+        ]
+
+    def get_customer_name(self, obj):
+        return obj.customer.name
+
+    def get_vendor_name(self, obj):
+        return obj.vendor.name
+
+    def get_total_value(self, obj):
+        return obj.get_total_value()
 
     def create(self, validated_data):
         products_data = validated_data.pop("saleproduct_set", [])
+        last_sale = Sale.objects.last()
+        validated_data["invoice_number"] = str(last_sale.id + 1 if last_sale else 1)
         sale = Sale.objects.create(**validated_data)
 
         for product_data in products_data:
