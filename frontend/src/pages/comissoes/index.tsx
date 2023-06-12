ec021400533a3eaa875a3c8ae7f36e509396d25f
Elder Santos <elder_rn@hotmail.com>
2023-06-12 16:05:32 -0300

    feat: finish commissions page

--- frontend/src/pages/comissoes/index.tsx
+++ frontend/src/pages/comissoes/index.tsx
@@ -1,22 +1,37 @@
 import SideBar from "@/components/SideBar";
 import TopBar from "@/components/TopBar";
+import Caption from "@/components/Caption";
 import useAppContext from "@/hooks/useAppContext";
 import { useEffect } from "react";
+import Container from "@/components/Container";
+import MainContent from "@/components/MainContent";
+import DatePeriodForm from "@/components/DatePeriodForm";
+import CommissionsTable from "@/components/CommissionsTable";
+import { CommissionsDataProvider } from "@/contexts/CommissionsDataContext";
+import PageActions from "@/components/PageActions";
 
 export default function CommissionsPage() {
+  const { setPageTitle, openSideBar } = useAppContext();
 
-  const { setPageTitle, openSideBar } = useAppContext()
-
-  useEffect(
-    () => {
-      setPageTitle("Comissões")
-    }, []
-  )
+  useEffect(() => {
+    setPageTitle("Comissões");
+  }, [setPageTitle]);
 
   return (
     <div>
       <TopBar />
-      { openSideBar && <SideBar /> }
+      <Container>
+        {openSideBar && <SideBar />}
+        <CommissionsDataProvider>
+          <MainContent>
+            <PageActions>
+              <Caption text="Relatório de Comissões" />
+              <DatePeriodForm />
+            </PageActions>
+            <CommissionsTable />
+          </MainContent>
+        </CommissionsDataProvider>
+      </Container>
     </div>
-  )
+  );
 }
