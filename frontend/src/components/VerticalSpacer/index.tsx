b77a5bca0248cd73cb13db46a599e25b06fa12ea
Elder Santos <elder_rn@hotmail.com>
2023-06-12 16:18:01 -0300

    feat: add VerticalSapcer component

---
+++ frontend/src/components/VerticalSpacer/index.tsx
@@ -0,0 +1,12 @@
+export default function VerticalSpacer() {
+  return (
+    <div
+      style={{
+        height: `85%`,
+        width: `1px`,
+        backgroundColor: `#C4C4C4`,
+        margin: `40px`,
+      }}
+    ></div>
+  );
+}
