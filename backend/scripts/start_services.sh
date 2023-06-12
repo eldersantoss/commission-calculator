95d26c2a1716817740937115fbbee42f3f3b2e6c
Elder Santos <elder_rn@hotmail.com>
2023-06-12 20:25:45 -0300

    build: fix docker compose start script

--- backend/scripts/start_services.sh
+++ backend/scripts/start_services.sh
@@ -2,29 +2,24 @@
 
 set -e
 
-PROCESS_TYPE=$1
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser_if_none_exists \
+  --username=$ADMIN_USERNAME \
+  --password=$ADMIN_PASSWORD \
+  --email=$ADMIN_EMAIL
 
-if [ "$PROCESS_TYPE" = "backend" ]; then
-  python manage.py makemigrations
-  python manage.py migrate
-  python manage.py createsuperuser_if_none_exists \
-    --username=$ADMIN_USERNAME \
-    --password=$ADMIN_PASSWORD \
-    --email=$ADMIN_EMAIL
+if [ "$DEBUG" = "True" ]; then
+  python manage.py runserver 0.0.0.0:8000
 
-  if [ "$DEBUG" = "True" ]; then
-    python manage.py runserver 0.0.0.0:8000
-
-  else
-    python manage.py collectstatic --noinput
-    gunicorn \
-    --bind 0.0.0.0:8000 \
-    --workers 2 \
-    --worker-class gevent \
-    --log-level DEBUG \
-    --access-logfile "-" \
-    --error-logfile "-" \
-    falatu.wsgi
-  fi
-
+else
+  python manage.py collectstatic --noinput
+  gunicorn \
+  --bind 0.0.0.0:8000 \
+  --workers 2 \
+  --worker-class gevent \
+  --log-level DEBUG \
+  --access-logfile "-" \
+  --error-logfile "-" \
+  falatu.wsgi
 fi
