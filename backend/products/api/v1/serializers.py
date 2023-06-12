10671bfe1255b29013b3df27932c24415720562e
Elder Santos <elder_rn@hotmail.com>
2023-06-12 17:12:09 -0300

    feat: add url field to ProductSerializer

--- backend/products/api/v1/serializers.py
+++ backend/products/api/v1/serializers.py
@@ -3,12 +3,14 @@ from rest_framework import serializers
 from ...models import Product
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = (
+            "url",
             "code",
             "description",
             "unit_price",
             "commission_rate",
         )
+        read_only_fields = ["url"]
