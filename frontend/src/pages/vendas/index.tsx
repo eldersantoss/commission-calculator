d4da9ab0e4d4568659bcac816e9e573fb8b06edd
Elder Santos <elder_rn@hotmail.com>
2023-06-12 16:11:23 -0300

    feat: add sales page

--- frontend/src/pages/vendas/index.tsx
+++ frontend/src/pages/vendas/index.tsx
@@ -1,22 +1,45 @@
 import SideBar from "@/components/SideBar";
 import TopBar from "@/components/TopBar";
+import Caption from "@/components/Caption";
 import useAppContext from "@/hooks/useAppContext";
 import { useEffect } from "react";
+import Container from "@/components/Container";
+import MainContent from "@/components/MainContent";
+import SalesTable from "@/components/SalesTable";
+import MainButton from "@/components/MainButton";
+import PageActions from "@/components/PageActions";
+import Link from "next/link";
+import MessagePopup from "@/components/MessagePopup";
 
 export default function SalesPage() {
+  const { setPageTitle, openSideBar, displayedMessagePopup, pageTitle } =
+    useAppContext();
 
-  const { setPageTitle, openSideBar } = useAppContext()
+  useEffect(() => {
+    if (pageTitle !== "Vendas") setPageTitle("Vendas");
+  }, [pageTitle, setPageTitle]);
 
-  useEffect(
-    () => {
-      setPageTitle("Vendas")
-    }, []
-  )
+  console.log("RENDERIZANDO TELA DE VENDAS...");
 
   return (
     <div>
       <TopBar />
-      { openSideBar && <SideBar /> }
+      <Container>
+        {openSideBar && <SideBar />}
+        <MainContent>
+          <PageActions>
+            <Caption text="Relatório de Vendas" />
+            {displayedMessagePopup ? (
+              <MessagePopup />
+            ) : (
+              <Link href="/vendas/alterar">
+                <MainButton content={"Inserir nova Venda"} />
+              </Link>
+            )}
+          </PageActions>
+          <SalesTable />
+        </MainContent>
+      </Container>
     </div>
-  )
+  );
 }
