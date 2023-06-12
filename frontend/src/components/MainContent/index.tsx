4691d2ab2e15e00eea30813a70d227ee5b537234
Elder Santos <elder_rn@hotmail.com>
2023-06-12 16:21:58 -0300

    refactor: update MainContent component format

--- frontend/src/components/MainContent/index.tsx
+++ frontend/src/components/MainContent/index.tsx
@@ -1,22 +1,23 @@
 import { ReactNode } from "react";
 
-
 interface MainContentProps {
   children: ReactNode;
 }
 
-export default function MainContent({ children }: MainContentProps ) {
+export default function MainContent({ children }: MainContentProps) {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      padding: "50px",
-      height: "100%",
-      width: "100%"
-    }}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "50px",
+        height: "100%",
+        width: "100%",
+      }}
+    >
       {children}
     </div>
-  )
+  );
 }
