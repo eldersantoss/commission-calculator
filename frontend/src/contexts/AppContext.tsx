aecf55feb239f14c6388b369743b41bb7ea3cf60
Elder Santos <elder_rn@hotmail.com>
2023-06-12 15:44:25 -0300

    feat: add AppContext

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,39 +1,41 @@
-import {Dispatch, SetStateAction, createContext, useState} from "react"
-
+import { Dispatch, SetStateAction, createContext, useState } from "react";
 
 interface AppContextProps {
-  pageTitle: string,
-  setPageTitle: Dispatch<SetStateAction<string>>,
-  openSideBar: boolean,
-  toggleSideBar: () => void
+  pageTitle: string;
+  setPageTitle: Dispatch<SetStateAction<string>>;
+  openSideBar: boolean;
+  toggleSideBar: () => void;
+  displayedMessagePopup: string;
+  setDisplayedMessagePopup: Dispatch<SetStateAction<string>>;
 }
 
 export const AppContext = createContext<AppContextProps>({
   pageTitle: "",
   setPageTitle: () => {},
   openSideBar: false,
-  toggleSideBar: () => {}
-})
+  toggleSideBar: () => {},
+  displayedMessagePopup: "",
+  setDisplayedMessagePopup: () => {},
+});
 
 export function AppProvider({ children }: any) {
-
-  const [pageTitle, setPageTitle] = useState<string>("Vendas")
-  const [openSideBar, setOpenSideBar] = useState<boolean>(false)
+  const [pageTitle, setPageTitle] = useState<string>("Vendas");
+  const [openSideBar, setOpenSideBar] = useState<boolean>(false);
+  const [displayedMessagePopup, setDisplayedMessagePopup] =
+    useState<string>("");
 
   function toggleSideBar() {
-    setOpenSideBar(!openSideBar)
+    setOpenSideBar(!openSideBar);
   }
 
   const values = {
     pageTitle,
     setPageTitle,
     openSideBar,
-    toggleSideBar
-  }
+    toggleSideBar,
+    displayedMessagePopup,
+    setDisplayedMessagePopup,
+  };
 
-  return (
-    <AppContext.Provider value={values}>
-      {children}
-    </AppContext.Provider>
-  )
+  return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
 }
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProvider } from '@/contexts/AppContext'
+import { SalesDataProvider } from '@/contexts/SalesDataContext'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppProvider>
-      <Component {...pageProps} />
+      <SalesDataProvider>
+        <Component {...pageProps} />
+      </SalesDataProvider>
     </AppProvider>
   )
 }
