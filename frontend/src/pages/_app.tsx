0a80e3ef682e6131f599f2a4bb2c30f24d0f25ae
Elder Santos <elder_rn@hotmail.com>
2024-06-22 21:33:45 -0300

    chore: configura react query

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,19 @@
-import { AppProvider } from '@/contexts/AppContext'
-import { SalesDataProvider } from '@/contexts/SalesDataContext'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProvider } from "@/contexts/AppContext";
+import { SalesDataProvider } from "@/contexts/SalesDataContext";
+import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppProvider>
-      <SalesDataProvider>
-        <Component {...pageProps} />
-      </SalesDataProvider>
-    </AppProvider>
-  )
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <SalesDataProvider>
+          <Component {...pageProps} />
+        </SalesDataProvider>
+      </AppProvider>
+    </QueryClientProvider>
+  );
 }
