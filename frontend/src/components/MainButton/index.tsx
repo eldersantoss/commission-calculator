554948ad26cd89dc16ba397845d6b0f4947d9ce3
Elder Santos <elder_rn@hotmail.com>
2024-10-21 15:43:15 -0300

    feat: transition on hover main button

--- frontend/src/components/MainButton/index.tsx
+++ frontend/src/components/MainButton/index.tsx
@@ -39,6 +39,7 @@ export default function MainButton({
     backgroundColor: buttonBackgroundColor,
     color: primaryFontColor,
     border: borderStyle,
+    transition: "background-color 0.3s",
   };
 
   return (
